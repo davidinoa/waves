a1892e026938b2ac4be8abff1a8baea3fcd6726b
David Inoa <davidinoa@gmail.com>
2019-01-05 15:31:47 -0400

    add wood and product routes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,22 +1,21 @@
 const { User } = require('./../models/user');
 
-let auth = (req, res, next) => {
-  let token = req.cookies.w_auth;
+let auth = (req,res,next) => {
+    let token = req.cookies.w_auth;
 
-  User.findByToken(token, (err, user) => {
-    if (err) throw err;
+    User.findByToken(token,(err,user)=>{
+        if(err) throw err;
+        if(!user) return res.json({
+            isAuth: false,
+            error: true
+        });
 
-    if (!user) {
-      return res.json({
-        isAuth: false,
-        error: true,
-      });
-    }
+        req.token = token;
+        req.user = user;
+        next();
+    })
 
-    req.token = token;
-    req.user = user;
-    next();
-  });
-};
+}
 
-module.exports = { auth };
+
+module.exports = { auth }
