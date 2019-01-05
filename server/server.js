a1222cca9faae63f0acadd8e7e69adb3309ce124
David Inoa <davidinoa@gmail.com>
2019-01-05 16:02:49 -0400

    apply prettier

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser'); 
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.DATABASE);
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
@@ -23,7 +23,6 @@ const { Product } = require('./models/product');
 const { auth } = require('./middleware/auth');
 const { admin } = require('./middleware/admin');
 
-
 //=================================
 //             PRODUCTS
 //=================================
@@ -33,169 +32,164 @@ const { admin } = require('./middleware/admin');
 
 // BY SELL
 // /articles?sortBy=sold&order=desc&limit=100&skip=5
-app.get('/api/product/articles',(req,res)=>{
+app.get('/api/product/articles', (req, res) => {
+  let order = req.query.order ? req.query.order : 'asc';
+  let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+  let limit = req.query.limit ? parseInt(req.query.limit) : 100;
 
-    let order = req.query.order ? req.query.order : 'asc';
-    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
-    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
-
-    Product.
-    find().
-    populate('brand').
-    populate('wood').
-    sort([[sortBy,order]]).
-    limit(limit).
-    exec((err,articles)=>{
-        if(err) return res.status(400).send(err);
-        res.send(articles)
-    })
-})
-
-
-/// /api/product/article?id=HSHSHSKSK,JSJSJSJS,SDSDHHSHDS,JSJJSDJ&type=single
-app.get('/api/product/articles_by_id',(req,res)=>{
-    let type = req.query.type;
-    let items = req.query.id;
-
-    if(type === "array"){
-        let ids = req.query.id.split(',');
-        items = [];
-        items = ids.map(item=>{
-            return mongoose.Types.ObjectId(item)
-        })
-    }
-
-    Product.
-    find({ '_id':{$in:items}}).
-    populate('brand').
-    populate('wood').
-    exec((err,docs)=>{
-        return res.status(200).send(docs)
-    })
+  Product.find()
+    .populate('brand')
+    .populate('wood')
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err, articles) => {
+      if (err) return res.status(400).send(err);
+      res.send(articles);
+    });
 });
 
+/// /api/product/article?id=HSHSHSKSK,JSJSJSJS,SDSDHHSHDS,JSJJSDJ&type=single
+app.get('/api/product/articles_by_id', (req, res) => {
+  let type = req.query.type;
+  let items = req.query.id;
 
-app.post('/api/product/article',auth,admin,(req,res)=>{
-    const product = new Product(req.body);
+  if (type === 'array') {
+    let ids = req.query.id.split(',');
+    items = [];
+    items = ids.map((item) => {
+      return mongoose.Types.ObjectId(item);
+    });
+  }
 
-    product.save((err,doc)=>{
-        if(err) return res.json({success:false,err});
-        res.status(200).json({
-            success: true,
-            article: doc
-        })
-    })
-})
+  Product.find({ _id: { $in: items } })
+    .populate('brand')
+    .populate('wood')
+    .exec((err, docs) => {
+      return res.status(200).send(docs);
+    });
+});
+
+app.post('/api/product/article', auth, admin, (req, res) => {
+  const product = new Product(req.body);
+
+  product.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      article: doc,
+    });
+  });
+});
 
 //=================================
 //              WOODS
 //=================================
 
-app.post('/api/product/wood',auth,admin,(req,res)=>{
-    const wood = new Wood(req.body);
+app.post('/api/product/wood', auth, admin, (req, res) => {
+  const wood = new Wood(req.body);
 
-    wood.save((err,doc)=>{
-        if(err) return res.json({success:false,err});
-        res.status(200).json({
-            success: true,
-            wood: doc
-        })
-    })
+  wood.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      wood: doc,
+    });
+  });
 });
 
-app.get('/api/product/woods',(req,res)=>{
-    Wood.find({},(err,woods)=>{
-        if(err) return res.status(400).send(err);
-        res.status(200).send(woods)
-    })
-})
-
+app.get('/api/product/woods', (req, res) => {
+  Wood.find({}, (err, woods) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(woods);
+  });
+});
 
 //=================================
 //              BRAND
 //=================================
 
-app.post('/api/product/brand',auth,admin,(req,res)=>{
-    const brand = new Brand(req.body);
+app.post('/api/product/brand', auth, admin, (req, res) => {
+  const brand = new Brand(req.body);
 
-    brand.save((err,doc)=>{
-        if(err) return res.json({success:false,err});
-        res.status(200).json({
-            success:true,
-            brand: doc
-        })
-    })
-})
+  brand.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      brand: doc,
+    });
+  });
+});
 
-app.get('/api/product/brands',(req,res)=>{
-    Brand.find({},(err,brands)=>{
-        if(err) return res.status(400).send(err);
-        res.status(200).send(brands)
-    })
-})
-
+app.get('/api/product/brands', (req, res) => {
+  Brand.find({}, (err, brands) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(brands);
+  });
+});
 
 //=================================
 //              USERS
 //=================================
 
-app.get('/api/users/auth',auth,(req,res)=>{
-        res.status(200).json({
-            isAdmin: req.user.role === 0 ? false : true,
-            isAuth: true,
-            email: req.user.email,
-            name: req.user.name,
-            lastname: req.user.lastname,
-            role: req.user.role,
-            cart: req.user.cart,
-            history: req.user.history
-        })
-})
-
-app.post('/api/users/register',(req,res)=>{
-    const user = new User(req.body);
-
-    user.save((err,doc)=>{
-        if(err) return res.json({success:false,err});
-        res.status(200).json({
-            success: true
-        })
-    })
+app.get('/api/users/auth', auth, (req, res) => {
+  res.status(200).json({
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    cart: req.user.cart,
+    history: req.user.history,
+  });
 });
 
-app.post('/api/users/login',(req,res)=>{
-    User.findOne({'email':req.body.email},(err,user)=>{
-        if(!user) return res.json({loginSuccess:false,message:'Auth failed, email not found'});
+app.post('/api/users/register', (req, res) => {
+  const user = new User(req.body);
 
-        user.comparePassword(req.body.password,(err,isMatch)=>{
-            if(!isMatch) return res.json({loginSuccess:false,message:'Wrong password'});
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+    });
+  });
+});
 
-            user.generateToken((err,user)=>{
-                if(err) return res.status(400).send(err);
-                res.cookie('w_auth',user.token).status(200).json({
-                    loginSuccess: true
-                })
-            })
-        })
-    })
-})
+app.post('/api/users/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: 'Auth failed, email not found',
+      });
 
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: 'Wrong password' });
 
-app.get('/api/user/logout',auth,(req,res)=>{
-    User.findOneAndUpdate(
-        { _id:req.user._id },
-        { token: '' },
-        (err,doc)=>{
-            if(err) return res.json({success:false,err});
-            return res.status(200).send({
-                success: true
-            })
-        }
-    )
-})
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie('w_auth', user.token)
+          .status(200)
+          .json({
+            loginSuccess: true,
+          });
+      });
+    });
+  });
+});
 
+app.get('/api/user/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
+});
 
 const port = process.env.PORT || 3002;
-app.listen(port,()=>{
-    console.log(`Server Running at ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server Running at ${port}`);
+});
