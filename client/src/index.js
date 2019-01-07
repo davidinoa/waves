982b4c6b353a4e94ea25cad324125a9d6147550f
David Inoa <davidinoa@gmail.com>
2019-01-07 13:59:50 -0400

    client init

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Resources/css/styles.css';
+
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(<BrowserRouter />, document.getElementById('root'));
